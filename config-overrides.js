602b75c29b5c3069f1e56271ef31c466ced40c1e
faizAhmed10 <faizahmed10604@gmail.com>
2024-08-06 20:12:18 +0530

    commit

--- config-overrides.js
+++ config-overrides.js
@@ -3,5 +3,7 @@ const { override, addBabelPlugin } = require('customize-cra');
 module.exports = override(
   addBabelPlugin(["@babel/plugin-transform-class-properties", { "loose": true }]),
   addBabelPlugin(["@babel/plugin-transform-private-methods", { "loose": true }]),
-  addBabelPlugin(["@babel/plugin-transform-private-property-in-object", { "loose": true }])
+  addBabelPlugin(["@babel/plugin-transform-private-property-in-object", { "loose": true }]),
+  addBabelPlugin("@babel/plugin-transform-nullish-coalescing-operator")
 );
+
