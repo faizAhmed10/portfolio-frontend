e85b24f58c703f86edccd38cbca9c3f7e15b153e
faizAhmed10 <faizahmed10604@gmail.com>
2024-08-06 19:11:23 +0530

    commit

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,7 @@
 const { override, addBabelPlugin } = require('customize-cra');
 
 module.exports = override(
-  addBabelPlugin('@babel/plugin-transform-private-property-in-object'),
-  addBabelPlugin('@babel/plugin-transform-private-methods')
+  addBabelPlugin(["@babel/plugin-transform-class-properties", { "loose": true }]),
+  addBabelPlugin(["@babel/plugin-transform-private-methods", { "loose": true }]),
+  addBabelPlugin(["@babel/plugin-transform-private-property-in-object", { "loose": true }])
 );
